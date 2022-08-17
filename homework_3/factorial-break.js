51ad1230fa7d6e1d057d40b6d58785187d5a41db
David Monsalve <davidmonsalve717@gmail.com>
2022-08-17 20:10:35 -0300

    second commit

---
+++ homework_3/factorial-break.js
@@ -0,0 +1,16 @@
+//Calculate the factorial of 10
+//using one while loop, one if sentence
+//and a break.
+
+n = 10
+i = 9
+
+while(i > 0){
+    n = n*i
+    i--
+    if (i === 1){
+        break
+    }
+}
+
+console.log("result: ", n)
---
+++ homework_3/factorial-while.js
@@ -0,0 +1,11 @@
+//Calculate de factorial of 10 using While
+
+var n = 10
+var i = 9
+
+while (i > 0){
+    n = n*i
+    i--
+}
+
+console.log("result: ", n)
