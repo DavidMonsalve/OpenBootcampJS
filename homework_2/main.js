b25fc477a11fc671b6dbc90fd6f70d499bfd1c5d
David Monsalve <davidmonsalve717@gmail.com>
2022-08-17 12:19:11 -0300

    first commit

---
+++ homework_2/main.js
@@ -0,0 +1,16 @@
+const myName = "David Monsalve";
+var myAge = 19;
+const amIaDev = true;
+const birthDate = new Date("October 15, 2002");
+const favBook = {
+    title: "The Art Of War.",
+    author: "Sun Tzu",
+    release: "5th century BC.",
+    url: "https://www.amazon.com/-/es/Sun-Tzu/dp/9388369696/ref=sr_1_2?__mk_es_US=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=3O87TB90XTLLU&keywords=the+art+of+war&qid=1660702586&s=books&sprefix=the+art+of+wa%2Cstripbooks-intl-ship%2C237&sr=1-2"
+};
+
+console.log(myName);
+console.log(myAge);
+console.log(amIaDev);
+console.log(birthDate);
+console.log(favBook);
