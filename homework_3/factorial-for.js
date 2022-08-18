2c7d2e381b465a6e8563c9f9fad6edcf4eb9b67b
David Monsalve <davidmonsalve717@gmail.com>
2022-08-17 22:12:11 -0300

    update

--- homework_3/factorial-for.js
+++ homework_3/factorial-for.js
@@ -1,9 +1,9 @@
 //Calculate the factorial of 10 using For
 
-var n = 10
+var n = 10;
 
 for (let i = n-1; i > 0; i--){
-    var n = n * i
+    var n = n * i;
 }
 
-console.log(n)
+console.log(n);
--- homework_3/factorial-while.js
+++ homework_3/factorial-while.js
@@ -1,11 +1,11 @@
 //Calculate de factorial of 10 using While
 
-var n = 10
-var i = 9
+var n = 10;
+var i = 9;
 
 while (i > 0){
-    n = n*i
-    i--
+    n = n*i;
+    i--;
 }
 
-console.log("result: ", n)
+console.log("result: ", n);
